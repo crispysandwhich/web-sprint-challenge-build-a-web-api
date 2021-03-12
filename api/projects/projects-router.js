e3d50a9c3ae7e1fd69035fabc55d0fdd6915e748
de_gentlem4n <th3g3n3lm4n@gmail.com>
2021-03-12 14:34:13 -0500

    server listing

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,12 @@
 // Write your "projects" router here!
+const express = require('express');
+const router = express.Router();
+
+const Project = require('./projects-model');
+
+router.get('/', (req, res) => {
+  console.log(Project)
+})
+
+
+module.exports = router;
